f4a945bf4d7b34ff2a40bc592a589696b14a12e8
Logan Chang <logiebear@ucla.edu>
2015-11-15 21:03:55 -0800

    Created initial structure.

---
+++ PicShare/Controllers/PhotoHomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoViewController.swift
+//  PicShare
+//
+//  Created by Logan Chang on 11/15/15.
+//  Copyright © 2015 USC. All rights reserved.
+//
+
+import UIKit
+
+class PhotoHomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
---
+++ PicShare/Controllers/TabBarViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TabBarViewController.swift
+//  PicShare
+//
+//  Created by Logan Chang on 11/15/15.
+//  Copyright © 2015 USC. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
