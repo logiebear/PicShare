3e84eb11f5424ad7537f74f4cb6dfc9924cb0851
kevinthesun <kevinthesunwy@gmail.com>
2015-11-29 03:44:23 -0800

    Account

--- PicShare/Controllers/SignUpViewController.swift
+++ PicShare/Controllers/SignUpViewController.swift
@@ -7,8 +7,14 @@
 //
 
 import UIKit
+import Parse
 
 class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var signUpEmailInput: UITextField!
+    @IBOutlet weak var signUpUserNameInput: UITextField!
+    @IBOutlet weak var signUpPasswordInput: UITextField!
+    @IBOutlet weak var signUpConfirmPasswordInput: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +26,47 @@ class SignUpViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //MARK: - User Actions
+    @IBAction func signUpSubmitButton(sender: UIButton) {
+        //Check empty field or inconsistent passwords
+        if signUpEmailInput.text == nil || signUpUserNameInput.text == nil || signUpPasswordInput.text == nil || signUpConfirmPasswordInput.text == nil {
+            let error = NSError(domain: "SuperSpecialDomain", code: -99, userInfo: [
+                NSLocalizedDescriptionKey: "Please fill out all the fields!"
+                ])
+            self.showErrorView(error)
+        }
+        else if signUpPasswordInput.text! != signUpConfirmPasswordInput.text! {
+            let error = NSError(domain: "SuperSpecialDomain", code: -99, userInfo: [
+                NSLocalizedDescriptionKey: "Two passwords are different!"
+                ])
+            self.showErrorView(error)
+        }
+        
+        let user = PFUser()
+        user.email = signUpEmailInput.text
+        user.username = signUpUserNameInput.text
+        user.password = signUpPasswordInput.text
+        user.signUpInBackgroundWithBlock { succeeded, error in
+            if (succeeded) {
+                //The registration was successful
+                self.performSegueWithIdentifier("RegistrationSuccessful", sender: nil)
+            } else if let error = error {
+                //Something bad has occurred
+                self.showErrorView(error)
+            }
+        }
+    }
+    
+    //MARK: - Helper
+    func showErrorView(error: NSError) {
+        let alertView = UIAlertController(title: "Error",
+            message: error.localizedDescription, preferredStyle: .Alert)
+        let OKAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        alertView.addAction(OKAction)
+        self.presentViewController(alertView, animated: true, completion: nil)
+    }
+    
     
 
 }
---
+++ PicShare/PicShare/Controllers/SignUpViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SignUpViewController.swift
+//  PicShare
+//
+//  Created by Logan Chang on 11/27/15.
+//  Copyright © 2015 USC. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
