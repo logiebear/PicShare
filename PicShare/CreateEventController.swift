51cb7c47b3d0a146d9205eb04350eba65a89e283
ZhouJiashun <zhoujiashun@ZhouJiashuns-MacBook-Air.local>
2016-02-02 20:22:22 -0800

    EventMgmt 1 draft

---
+++ PicShare/CreateEventController.swift
@@ -0,0 +1,78 @@
+//
+//  CreateEventController.swift
+//  PicShare
+//
+//  Created by ZhouJiashun on 1/29/16.
+//  Copyright © 2016 USC. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class CreateEventController: UIViewController, UITextFieldDelegate {
+
+    // Mark: - Properties
+    
+    @IBOutlet weak var createEventButton: UIButton!
+    @IBOutlet weak var eventNameTextField: UITextField!
+    
+    var hashtag: String = " "
+    var user: PFUser = PFUser.currentUser()!
+    var isPublic: Bool = true
+    var password: String? = nil
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        
+        // Handle the text field's user input through delegate callbacks.
+        eventNameTextField.delegate = self
+    }
+    
+    // Mark: - UITextFieldDelegate
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        // Hide the keyboard
+        eventNameTextField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        if let text = eventNameTextField.text{
+            hashtag = text
+        }else{
+            // TO DO: - Show error message
+        }
+    }
+    
+    // Mark: - User Actions
+    
+    @IBAction func createEventButtonPressed(sender: AnyObject) {
+        
+        self.createEventObject()
+        
+    }
+    
+    // Mark: - Private
+    
+    private func createEventObject() {
+        
+        let event = Event(owner: user,
+            hashtag: hashtag,
+            isPublic: isPublic,
+            password: password
+        )
+        
+       event.saveInBackgroundWithBlock({ [weak self]
+            (success, error) -> Void in
+            //self?.activityIndicatorViewControllerAnimating()
+            self?.dismissViewControllerAnimated(true, completion: nil)
+        })
+    }
+
+    // Mark: - Helper
+    
+
+}
