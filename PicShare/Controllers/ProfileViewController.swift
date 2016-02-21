d12e166ecce5920c38e783be564d315a4fb7d828
Logan Chang <logiebear@ucla.edu>
2016-02-20 18:29:55 -0800

    Adding profile image.

--- PicShare/Controllers/ProfileViewController.swift
+++ PicShare/Controllers/ProfileViewController.swift
@@ -8,14 +8,38 @@
 
 import UIKit
 import Parse
+import ParseUI
 
 class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var profileImageView: PFImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-      print("Hello")
+        
+        let query = PFQuery(className: User.parseClassName())
+        query.includeKey("owner")
+        query.orderByDescending("createdAt")
+        query.findObjectsInBackgroundWithBlock { [weak self](objects: [PFObject]?, error: NSError?) -> Void in
+            
+            if let error = error {
+                print("Error: \(error) \(error.userInfo)")
+                return
+            }
+            
+            guard let users = objects as? [User] else {
+                return
+            }
+            
+            for user in users {
+                if user.objectId == PFUser.currentUser()?.objectId {
+                    self?.profileImageView.file = user.profilePhoto
+                    self?.profileImageView.loadInBackground()
+                }
+            }
+        }
     }
 
     @IBAction func logoutButtonPressed(sender: AnyObject) {
