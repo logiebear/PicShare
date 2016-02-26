98aebb032aba3eeab545b65e99cf794b39e10930
Nick Funk <nfunk@usc.edu>
2016-02-26 14:38:17 -0800

    Initial UI transitions for profile editing

--- PicShare/Controllers/ProfileViewController.swift
+++ PicShare/Controllers/ProfileViewController.swift
@@ -14,6 +14,10 @@ class ProfileViewController: UIViewController {
     
     @IBOutlet weak var profileImageView: PFImageView!
     @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var userInfoImageView: UIView!
+    @IBOutlet weak var userInfoImageViewHeight: NSLayoutConstraint!
+    
+    private var modifyButtonsVisible = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,9 +53,42 @@ class ProfileViewController: UIViewController {
                     }
                 }
             }
-            
-            
         }
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        if (modifyButtonsVisible) {
+            hideButtons()
+        }
+    }
+    
+    private func hideButtons() {
+//        print("Visible. Size of superview: \(self.userInfoImageView.frame.size.height)")
+//        self.profileImageView.layer.cornerRadius = self.profileImageView.frame.size.height
+//        self.profileImageView.frame.size.height = self.profileImageView.frame.size.height*2
+//        self.profileImageView.frame.size.width = self.profileImageView.frame.size.width*2
+//        self.userInfoImageView.frame.size.height = self.userInfoImageView.frame.size.height*2
+//        print("Hiding. Size of superview: \(self.userInfoImageView.frame.size.height)")
+        self.userInfoImageViewHeight.constant = 300
+        UIView.animateWithDuration(0.5) {
+            self.view.layoutIfNeeded()
+        }
+        modifyButtonsVisible = false
+    }
+    
+    private func showButtons() {
+//        print("Hidden. Size of superview: \(self.userInfoImageView.frame.size.height)")
+//        self.profileImageView.frame.size.height = self.profileImageView.frame.size.height/2
+//        self.profileImageView.frame.size.width = self.profileImageView.frame.size.width/2
+//        self.profileImageView.layer.cornerRadius = self.profileImageView.frame.size.height/2
+//        self.userInfoImageView.frame.size.height = self.userInfoImageView.frame.size.height/2
+//          print("Visifying. Size of superview: \(self.userInfoImageView.frame.size.height)")
+        self.userInfoImageViewHeight.constant = 150
+        UIView.animateWithDuration(0.5) {
+            self.view.layoutIfNeeded()
+        }
+        modifyButtonsVisible = true
     }
 
     @IBAction func logoutButtonPressed(sender: AnyObject) {
@@ -59,4 +96,16 @@ class ProfileViewController: UIViewController {
         NSNotificationCenter.defaultCenter().postNotificationName(accountStatusChangedNotification, object: nil)
     }
     
+   
+    @IBAction func profilePhotoTapped(sender: UITapGestureRecognizer) {
+        // Things I need to do
+        // 1. Toggle photo size between large and less large
+        if (modifyButtonsVisible) {
+            hideButtons()
+
+        } else {
+            showButtons()
+        }
+        // 2. Toggle three buttons visible or not visible
+    }
 }
